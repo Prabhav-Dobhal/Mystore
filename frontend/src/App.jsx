3615547862d221062112b85d5de2185b076b4e58
Prabhav-Dobhal <93513855+Prabhav-Dobhal@users.noreply.github.com>
2024-02-05 22:34:25 +0530

    Added landscape App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/home";
 import Cart from "./components/Cart/cart";
@@ -11,20 +11,48 @@ import OrderFailed from "./components/order/orderfailed";
 import Myorder from "./components/Myorders/myorder";
 
 function App() {
+  const [isPortrait, setIsPortrait] = useState(
+    window.matchMedia("(orientation: portrait)").matches
+  );
+
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      setIsPortrait(window.matchMedia("(orientation: portrait)").matches);
+    };
+
+    window.addEventListener("resize", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("resize", handleOrientationChange);
+    };
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <UserProvider>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/search" element={<Searchproductpage />} />
-            <Route path="/viewproduct" element={<ViewProduct />} />
-            <Route path="/address" element={<Address />} />
-            <Route path="/success" element={<Orderconfirmed />} />
-            <Route path="/cancel" element={<OrderFailed />} />
-            <Route path="/myorder" element={<Myorder />} />
-          </Routes>
+          {isPortrait === true ? (
+            <img
+              src="/landscape_mode.png"
+              style={{
+                marginTop: "20vh",
+                width: "100vw",
+                height: "45vh",
+                backgroundSize: "cover",
+              }}
+            ></img>
+          ) : (
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/search" element={<Searchproductpage />} />
+              <Route path="/viewproduct" element={<ViewProduct />} />
+              <Route path="/address" element={<Address />} />
+              <Route path="/success" element={<Orderconfirmed />} />
+              <Route path="/cancel" element={<OrderFailed />} />
+              <Route path="/myorder" element={<Myorder />} />
+            </Routes>
+          )}
         </UserProvider>
       </div>
     </Router>
